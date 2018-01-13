b438e4d77b487c660fddc7293dcbb53154f591df
chris <dwatow@gmail.com>
2018-01-13 22:52:58 +0800

    modify body json init

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+
 
 app.get('/', function (req, res) {
     res.statusCode = 200;
