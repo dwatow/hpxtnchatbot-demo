4753fe75b031a118010cc74aa2ad38429d5846a8
chris <dwatow@gmail.com>
2018-01-13 19:16:52 +0800

    add log

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const PORT = 3000;
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(req.query['hub.challenge']);
-  console.log(req.url);
-  console.log(req.method);
+  res.end(`hello world`);
+  console.log('---------------------');
+  console.log('req.query', req.query);
+  console.log('---------------------');
+  console.log('req.url', req.url);
+  console.log('---------------------');
+  console.log('req.method', req.method);
 });
 
-
 server.listen(PORT);
-// server.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server running at http://${hostname}:${PORT}/`);
-// });
