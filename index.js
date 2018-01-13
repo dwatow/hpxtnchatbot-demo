91db9370d4d4ab2ffd21d9cac7e685bbd095fffc
chris <dwatow@gmail.com>
2018-01-13 21:27:15 +0800

    add facebook messager

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ const http = require('http');
 const url = require('url');
 const PORT = process.env.PORT || 3000;
 
+
+const router = function () {
+    var url2function = {};
+    url2function['/'] = root;
+    return url2function;
+}
+
+
 const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const currUrl = url.parse(req.url, true);
+  // res.end(`hello world`);
+  router[req.url](req, res);
+  console.log('---------------------');
+  console.log('req.method', req.method);
+  console.log('parameters', currUrl.query);
+  res.end(currUrl.query["hub.challenge"]);
+  // console.log('---------------------');
+  // console.log('req.url["name"]', req.url["name"]);
+});
+
+
+
+function root (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   // res.end(`hello world`);
@@ -11,6 +35,6 @@ const server = http.createServer((req, res) => {
   console.log('parameters', url.parse(req.url, true).query);
   // console.log('---------------------');
   // console.log('req.url["name"]', req.url["name"]);
-});
+}
 
 server.listen(PORT);
