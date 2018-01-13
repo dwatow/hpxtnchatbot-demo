f75b744a0fce542a3b91b2583ea0d9c96930f2b1
chris <dwatow@gmail.com>
2018-01-13 22:29:33 +0800

    change to express

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-const http = require('http');
-const url = require('url');
+
+// const request = require('request');
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  const currUrl = url.parse(req.url, true);
-  // res.end(`hello world`);
-  console.log('---------------------');
-  console.log('req.method', req.method);
-  console.log('parameters', currUrl.query);
-  console.log('token', currUrl.query["hub.challenge"]);
-  res.end(currUrl.query["hub.challenge"]);
-  // console.log('---------------------');
-  // console.log('req.url["name"]', req.url["name"]);
+var express = require('express');
+var app = express();
+
+app.get('/name', function (req, res) {
+    res.statusCode = 200;
+    res.send('hellow');
+    console.log(req.body);
 });
 
-
-
-function root (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  // res.end(`hello world`);
-  console.log('---------------------');
-  console.log('req.method', req.method);
-  console.log('parameters', url.parse(req.url, true).query);
-  // console.log('---------------------');
-  // console.log('req.url["name"]', req.url["name"]);
-}
-
-server.listen(PORT);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
