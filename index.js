1e10aff1e494836d8944eff91e514e3bf7683a04
chris <dwatow@gmail.com>
2018-01-13 16:14:04 +0800

    add for facebook bot

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-  console.log(req);
+  res.end(req.query['hub.challenge']);
+  console.log(req.url);
+  console.log(req.method);
 });
 
 
