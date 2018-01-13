0acd5e0e5c955495eb7f81fc9999a10e82ae7ef8
chris <dwatow@gmail.com>
2018-01-13 22:32:11 +0800

    change to express port

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 
-app.get('/name', function (req, res) {
+app.get('/', function (req, res) {
     res.statusCode = 200;
     res.send('hellow');
     console.log(req.body);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
 });
