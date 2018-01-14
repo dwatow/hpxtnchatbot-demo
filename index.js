efe03eb2e4f12092d188afd7ba56bbc268085a78
chris <dwatow@gmail.com>
2018-01-14 11:08:25 +0800

    fix res status code

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ app.use(bodyParser.json()); // for parsing application/json
 
 
 app.get('/', function (req, res) {
-    res.statusCode = 200;
-    res.send('hellow');
+    res.statusCode = 201;
+    res.send('Hallow');
     // console.log(req.body);
 });
 
 
 app.post('/', function (req, res) {
-    const url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAaPxZCnJiZCIBAN6KP15xd660iz5YBh5Wy4kDNEhHIflmtk8fR1dZB2gYdvtcZC4fHI0rZBGBfl4mZA4v9ZAx4UDBML5rCkk4Vun7lUMnodigokyOfyM9qSjXZBD4JiqIrUnym4SX08ixB8lFNZChVG4fBmCf40jGfeeTZCiTTOMkAwZDZD'
+
+    const url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAaPxZCnJiZCIBAN6KP15xd660iz5YBh5Wy4kDNEhHIflmtk8fR1dZB2gYdvtcZC4fHI0rZBGBfl4mZA4v9ZAx4UDBML5rCkk4Vun7lUMnodigokyOfyM9qSjXZBD4JiqIrUnym4SX08ixB8lFNZChVG4fBmCf40jGfeeTZCiTTOMkAwZDZD';
     res.statusCode = 200;
 
     const messaging = req.body.entry[0].messaging[0]
@@ -29,7 +30,8 @@ app.post('/', function (req, res) {
     request({
         url: url,
         method: "POST",
-        json: {
+        json: true,
+        multipart: {
           "recipient": {
             "id": messaging.sender
           },
