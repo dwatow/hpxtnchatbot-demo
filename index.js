5539848a0ef02ab9817d6539e3469b19cafe6263
chris <dwatow@gmail.com>
2018-01-14 11:45:17 +0800

    add verifyToken and messagingHandler

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
 
 const request = require('request');
-const PORT = process.env.PORT || 3000;
 
-const express = require('express');
-const app = express();
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); // for parsing application/json
-
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 app.get('/', function (req, res) {
     res.statusCode = 201;
@@ -15,33 +13,105 @@ app.get('/', function (req, res) {
     // console.log(req.body);
 });
 
+//
+// app.post('/', function (req, res) {
+//
+//     const url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAaPxZCnJiZCIBAN6KP15xd660iz5YBh5Wy4kDNEhHIflmtk8fR1dZB2gYdvtcZC4fHI0rZBGBfl4mZA4v9ZAx4UDBML5rCkk4Vun7lUMnodigokyOfyM9qSjXZBD4JiqIrUnym4SX08ixB8lFNZChVG4fBmCf40jGfeeTZCiTTOMkAwZDZD';
+//     res.statusCode = 200;
+//
+//     const messaging = req.body.entry[0].messaging[0]
+//     const echo = messaging.message.text;
+//
+//     // res.send(`聊天機器人測試功能: ${echo}`);
+//
+//     console.log('messaging: ', messaging);
+//     request({
+//         url: url,
+//         method: "POST",
+//         json: true,
+//         multipart: {
+//           "recipient": {
+//             "id": messaging.sender
+//           },
+//           "message": {
+//             "text": echo
+//           }
+//         }
+//     });
+// });
 
-app.post('/', function (req, res) {
-
-    const url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAaPxZCnJiZCIBAN6KP15xd660iz5YBh5Wy4kDNEhHIflmtk8fR1dZB2gYdvtcZC4fHI0rZBGBfl4mZA4v9ZAx4UDBML5rCkk4Vun7lUMnodigokyOfyM9qSjXZBD4JiqIrUnym4SX08ixB8lFNZChVG4fBmCf40jGfeeTZCiTTOMkAwZDZD';
-    res.statusCode = 200;
-
-    const messaging = req.body.entry[0].messaging[0]
-    const echo = messaging.message.text;
-
-    // res.send(`聊天機器人測試功能: ${echo}`);
-
-    console.log('messaging: ', messaging);
-    request({
-        url: url,
-        method: "POST",
-        json: true,
-        multipart: {
-          "recipient": {
-            "id": messaging.sender
-          },
-          "message": {
-            "text": echo
-          }
-        }
-    });
+// Creates the endpoint for our webhook
+app.post('/', (req, res) => {
+    verifyToken(req, res);
+    messagingHandler(req, res);
 });
 
+function  verifyToken (req, res) {
+    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+}
+
+function messagingHandler (req, res) {
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+}
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
