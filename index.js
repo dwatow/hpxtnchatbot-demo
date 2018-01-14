cb1808b713b2cc42b95814615f055ef1b17d29c1
chris <dwatow@gmail.com>
2018-01-14 19:58:47 +0800

    env for nodejs

--- index.js
+++ index.js
@@ -107,8 +107,38 @@ function handleMessage(page_id, sender_psid, received_message) {
         }
 
         // Sends the response message
-        callSendAPI(page_id, sender_psid, response);
     }
+    else if(received_message.attachments) {
+
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [{
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    callSendAPI(page_id, sender_psid, response);
 }
 
 // Handles messaging_postbacks events
