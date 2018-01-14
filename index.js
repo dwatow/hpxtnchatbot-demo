a553f8a2011463f9d3be14dafb0b7c699dca9811
chris <dwatow@gmail.com>
2018-01-14 12:01:23 +0800

    add echo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json());
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json());
 
 const request = require('request');
 
@@ -46,59 +46,116 @@ app.post('/', (req, res) => {
     messagingHandler(req, res);
 });
 
-function  verifyToken (req, res) {
+function verifyToken(req, res) {
     let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
+    // Checks if a token and mode is in the query string of the request
+    if(mode && token) {
 
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        // Checks the mode and token sent is correct
+        if(mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
 
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+        }
+        else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 }
 
-function messagingHandler (req, res) {
-  let body = req.body;
+function messagingHandler(req, res) {
+    let body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+    // Checks this is an event from a page subscription
+    if(body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if(webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            }
+            else if(webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    }
+    else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 
 }
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Check if the message contains text
+    if(received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {
+            "access_token": PAGE_ACCESS_TOKEN
+        },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if(!err) {
+            console.log('message sent!')
+        }
+        else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
 
 // Handles messaging_postbacks events
@@ -111,7 +168,8 @@ function callSendAPI(sender_psid, response) {
 
 }
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
+    console.log(`Example app listening on port ${PORT}!`);
 });
