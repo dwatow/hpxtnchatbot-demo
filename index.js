d835a021eeb0b617d1ff7655b7bbce78c4565e62
chris <dwatow@gmail.com>
2018-01-17 10:45:55 +0800

    add generic tamplate

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const
 const request = require('request');
 
 const pageToken = {
-    // "552871335047242": "EAAaPxZCnJiZCIBALw0ftSE1bzG9GK6NY60x9QTTAaoTFZBSqwucuRXz0VAxWd0X4STzYbS9EUnmamqwL3RrvXeOQNpx1Wx9Tz1ZAiOhfXZA21DthVMuCZAcfESdhWO5zO4Mvgj9dlE70MUvPEGCdyZArS71CkvnFlkL2uHCbBHAZBwZDZD",
     "270024299718336": "EAAaPxZCnJiZCIBAGGzxZBVPnegT78X4v7lrnJQAdQtuFUvjdEpPUnAMPAqMziBMHPIAbH60tkmlZBKgUW1ri7rsX5lqg3ZBSznPjYHsp9z34eUDvqeZA3YrZAjJWEYbt32H7bZCwKmfXK2JZCMyCi1JOYshvIcQlY6XHRvkNiUsPOYQZDZD",
-    "552871335047242": "EAACznxPYmxIBAEuZCkojFoo2Rb3ay3ZCb1ldESS8fgakTKcY2ZC7LLGHcI4y8TpgKMq73FEGACEMgfn0ccMuL9WaHbRG3taWYdfgLx11Yr8bctn9Yyd17ZAOi8ZCSCIn6yhC8aZAygZC516MzunaAxrDF4ZC6ljeGZB1CTdUWS6UaOgZDZD"
+    "552871335047242": "EAACznxPYmxIBAINPpe9alhwvOvujhu3Hb2ZCLfpK1luvQXosjViZAAfbKEFAjYncV7ZCfcqslEOTpYbxbGNGIoQ2G0igQwJZCfkOZCIljUBs0LHztZCtWe9Ozg32GeYY3vWHniiJ47E80IPcKM1BOcuDJfK9XutZBLlMMgTVQkKYAZDZD"
 }
 
 app.get('/', verifyToken);
@@ -46,11 +45,11 @@ function verifyToken(req, res) {
 
 // Creates the endpoint for our webhook
 app.post('/', (req, res) => {
-    messagingHandler(req, res);
+    handleTalking(req, res);
 });
 
 
-function messagingHandler(req, res) {
+function handleTalking(req, res) {
     let body = req.body;
     // console.log(body.object);
 
@@ -63,7 +62,10 @@ function messagingHandler(req, res) {
             // Gets the message. entry.messaging is an array, but
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
-            // console.log(webhook_event);
+            console.log('---------------');
+            // console.log('webhook event: ');
+            console.log(JSON.stringify(webhook_event));
+            console.log('---------------');
 
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
@@ -74,13 +76,16 @@ function messagingHandler(req, res) {
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
+
             // console.log(webhook_event.message);
-            senderAction(page_id, sender_psid, webhook_event.message);
+            // senderAction(page_id, sender_psid, webhook_event.message);
 
             if(webhook_event.message) {
+                console.log('message');
                 handleMessage(page_id, sender_psid, webhook_event.message);
             }
             else if(webhook_event.postback) {
+                console.log('post back');
                 handlePostback(page_id, sender_psid, webhook_event.postback);
             }
         });
@@ -94,7 +99,7 @@ function messagingHandler(req, res) {
     }
 }
 
-function senderAction (page_id, sender_psid, received_message) {
+function senderAction(page_id, sender_psid, received_message) {
     // Sends the response message
     let request_body = {
         "messaging_type": "RESPONSE",
@@ -103,7 +108,7 @@ function senderAction (page_id, sender_psid, received_message) {
         },
         "sender_action": "typing_on"
     }
-    console.log(request_body);
+    // console.log(request_body);
 
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -114,7 +119,7 @@ function senderAction (page_id, sender_psid, received_message) {
         "json": request_body
     }, (err, res, body) => {
         if(!err) {
-            console.log('message sent!')
+            console.log('sender action!');
         }
         else {
             console.error("Unable to send message:" + err);
@@ -124,67 +129,242 @@ function senderAction (page_id, sender_psid, received_message) {
 
 // Handles messages events
 function handleMessage(page_id, sender_psid, received_message) {
-    let response;
+    let respons_message = {};
 
     console.log('handleMessage: ', received_message.text);
 
     // Check if the message contains text
     if(received_message.text) {
-        // Create the payload for a basic text message
-        response = {
-            "text": `回音: "${received_message.text}"...`
+
+
+        const msg_text = received_message.text;
+        if((/看地圖|找地點|在哪|在哪裡/ig).test(msg_text)) {
+            console.log('quick location');
+
+            respons_message["text"] = `在地圖指一下你在哪`
+            respons_message["quick_replies"] = [{
+                "content_type": "location"
+            }]
+        }
+        else if((/看訊息範本/ig).test(msg_text)) {
+            respons_message["text"] = '你想要看哪一種訊息範本？'
+
+            // quick_replies
+            respons_message["quick_replies"] = [{
+                    "content_type": "text",
+                    "title": "一般型範本",
+                    "payload": "Generic template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "清單範本",
+                    "payload": "List template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "按鈕範本",
+                    "payload": "Button template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "開放社交關係圖範本",
+                    "payload": "Open Graph template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "收據範本",
+                    "payload": "Receipt template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "航空公司範本",
+                    "payload": "Airline templates",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "媒體範本",
+                    "payload": "Media template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                }
+            ]
+        }
+        else if((/一般型範本|清單範本|按鈕範本|開放社交關係圖範本|收據範本|航空公司範本|媒體範本/ig).test(msg_text)) {
+            let template = {
+                "type": "template"
+            };
+            respons_message["attachment"] = template;
+
+            if(msg_text == "一般型範本") {
+                let genericTemplate = {
+                    "title": "一般型範本",
+                    "image_url": "http://lorempixel.com/400/200/food/",
+                    "subtitle": "可以幫助你找到你想找的",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://www.facebook.com",
+                        "messenger_extensions": false,
+                        "webview_height_ratio": "FULL"
+                    },
+                    "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.google.com",
+                            "title": "Google",
+                        },
+                        {
+                            "type": "web_url",
+                            "url": "https://www.yahoo.com",
+                            "title": "Yahoo!",
+                        },
+                        {
+                            "type": "web_url",
+                            "url": "https://www.bing.com",
+                            "title": "Bing",
+                        }
+                    ]
+                };
+
+                template["payload"] = {
+                    "template_type": "generic",
+                    "elements": [
+                        genericTemplate
+                    ]
+                };
+            }
+            else if(msg_text == "清單範本") {
+                let listTemplate = {
+                    "title": "清單範本",
+                    "subtitle": "副標題",
+                    "image_url": "http://lorempixel.com/800/500/food/",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://www.facebook.com",
+                        "messenger_extensions": false,
+                        "webview_height_ratio": "COMPACT"
+                    },
+                    "buttons": [{
+                        "title": "View",
+                        "type": "web_url",
+                        "url": "https://www.facebook.com",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        // "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }]
+                }
+
+                let listTemplate2 = {
+                    "title": "Classic Blue T-Shirt",
+                    "image_url": "http://lorempixel.com/800/500/food/",
+                    "subtitle": "100% Cotton, 200% Comfortable",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://www.facebook.com",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://www.facebook.com"
+                    },
+                    "buttons": [{
+                        "title": "Shop Now",
+                        "type": "web_url",
+                        "url": "https://www.facebook.com",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://www.facebook.com"
+                    }]
+                }
+
+
+                template["payload"] = {
+                    "template_type": "generic",
+                    "elements": [
+                        listTemplate
+                    ]
+                }
+            }
+            else {
+                respons_message["text"] = "這個還沒有實作!!!";
+            }
+        }
+        else {
+            respons_message["text"] = `回音: "${msg_text}"...`
+            // quick_replies
+            respons_message["quick_replies"] = [{
+                    "content_type": "text",
+                    "title": "看地圖",
+                    "payload": "看地圖",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                },
+                {
+                    "content_type": "text",
+                    "title": "看訊息範本",
+                    "payload": "template",
+                    "image_url": "http://lorempixel.com/400/200/food/"
+                }
+            ]
         }
     }
     else if(received_message.attachments) {
         // Gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
-        response = {
+        respons_message = {
             "attachment": {
                 "type": "template",
                 "payload": {
                     "template_type": "generic",
-                    "elements": [
-                        {
-                            "title": "Is this the right picture?",
-                            "subtitle": "Tap a button to answer.",
-                            "image_url": attachment_url,
-                            "buttons": [{
-                                    "type": "postback",
-                                    "title": "Yes!",
-                                    "payload": "yes",
-                                },
-                                {
-                                    "type": "postback",
-                                    "title": "No!",
-                                    "payload": "no",
-                                }
-                            ],
-                        }
-                    ]
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [{
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
                 }
             }
         }
     }
 
+    console.log('-----------------');
+    console.log(JSON.stringify(respons_message));
+    console.log('-----------------');
+
     // Sends the response message
-    callSendAPI(page_id, sender_psid, response);
+    callSendAPI(page_id, sender_psid, respons_message);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(page_id, sender_psid, received_postback) {
     let response;
 
-     // Get the payload for the postback
-     let payload = received_postback.payload;
+    // Get the payload for the postback
+    let payload = received_postback.payload;
 
-     // Set the response based on the postback payload
-     if (payload === 'yes') {
-       response = { "text": "Thanks!" }
-     } else if (payload === 'no') {
-       response = { "text": "Oops, try sending another image." }
-     }
-     // Send the message to acknowledge the postback
-     callSendAPI(page_id, sender_psid, response);
+    // Set the response based on the postback payload
+    if(payload === 'yes') {
+        response = {
+            "text": "Thanks!"
+        }
+    }
+    else if(payload === 'no') {
+        response = {
+            "text": "Oops, try sending another image."
+        }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(page_id, sender_psid, response);
 }
 
 // Sends response messages via the Send API
@@ -198,6 +378,7 @@ function callSendAPI(page_id, sender_psid, response) {
         },
         "message": response
     }
+    console.log(request_body);
 
     // Send the HTTP request to the Messenger Platform
     request({
@@ -209,7 +390,7 @@ function callSendAPI(page_id, sender_psid, response) {
         "json": request_body
     }, (err, res, body) => {
         if(!err) {
-            console.log('message sent!')
+            console.log('message sent!');
         }
         else {
             console.error("Unable to send message:" + err);
